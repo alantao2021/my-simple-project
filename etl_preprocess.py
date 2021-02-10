5e617fbee1661d040813985055faa3149342e0c0
alan <taoyuanping@gmail.com>
2021-02-11 00:30:32 +1100

    modified

--- etl_preprocess.py
+++ etl_preprocess.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 import pandas as pd
 print('this is the work done by developer two')
-=======
-import os
-import json
-
-print('This is the work done by developer one')
->>>>>>> master
